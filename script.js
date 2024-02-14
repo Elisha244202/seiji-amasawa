cab5e2fc8dcbb944651f2846d6b40183dd2666c4
Elisha244202 <160017098+Elisha244202@users.noreply.github.com>
2024-02-14 16:25:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const navLink = document.querySelector('.header .nav-bar .nav-links');
+
+function showMenu() {
+  navLink.style.width = '200px';
+}
+
+function hideMenu() {
+  navLink.style.width = '0';
+}
